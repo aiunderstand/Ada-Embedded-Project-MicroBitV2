a041cec6effb8703a8f05dd8180237a909111f90
Stian N <stiannordholm@gmail.com>
2023-10-12 12:14:57 +0200

    Added lines of code to make the car drive forward

--- Project/src/main.adb
+++ Project/src/main.adb
@@ -1,27 +1,35 @@
-with MicroBit.Console; use MicroBit.Console;
-use MicroBit;
--- USN PROJECT TEMPLATE INTELLIGENT REAL-TIME SYSTEMS
--- Project name: [project name]
--- Project members: [name, name, .. ]
+with MicroBit.IOsForTasking;  use MicroBit.IOsForTasking;
+with MicroBit;
+with MicroBit.MotorDriver; use MicroBit.MotorDriver; --using the procedures defined here
+with DFR0548;  -- using the types defined here
+with MicroBit.Console; use MicroBit.Console; -- for serial port communication
+use MicroBit; --for pin names
 
-
---This is a project template for a Jorvik profile (a less restrictive Ravenscar) for the MicroBit v2 such that we have a language supported real-time OS for embedded targets
---It requires the nRF52833 Jorvik runtime files. They can be found in the folder "runtime profiles" and need to be copied to your earm-eabi compiler toolchain
---In future version of GNATstudio this profile is automatically available and this step can be skipped (for status see: https://github.com/AdaCore/bb-runtimes/pull/67)
-
---Check out the many examples in the ADL for the MicroBit v2 to see:
---  1) How the various drivers can be used like the accelerometer,speaker  or wireless communication via radio
---  2) How ADA features like the Math library and Bounded_Vectors (Containers) can be used
---  3) How to structure your Ada tasks with a protected object to synchronize data or with entries to synchronize flow
---  4) How to perform a execution time analysis
---  5) How to integrate your Ada project with a Unity project over USB
-
--- Open a View > Cross Platforms > Serial Ports to see Put_Line output. Set the baud rate to 115.200
-procedure Main with Priority => 0 is
+procedure Main is
 
 begin
-   Put_Line (" <-- The zero means: Let's get started ...");
+   MotorDriver.Servo(1,90);
+   delay 1.0; -- equivalent of Time.Sleep(1000) = 1 second
+
    loop
-      null;
+      --   DEMONSTRATION ROUTINE 4 MOTORS (useful for checking your wiring)
+      --  MotorDriver.Drive(Forward,(4095,0,0,0)); --right front wheel to M4
+      --  delay 1.0;
+      --  MotorDriver.Drive(Forward,(0,4095,0,0)); --right back wheel to M3
+      --  delay 1.0;
+      --  MotorDriver.Drive(Forward,(0,0,4095,0)); --left front wheel to M2
+      --  delay 1.0;
+      --  MotorDriver.Drive(Forward,(0,0,0,4095)); --left back wheel to M1
+      --  delay 1.0;
+      --  MotorDriver.Drive(Stop);
+
+
+      MotorDriver.Drive(Forward);
+      delay 1.0;
+      MotorDriver.Drive(stop);
+
+      delay 3.0;
+
+
    end loop;
 end Main;
